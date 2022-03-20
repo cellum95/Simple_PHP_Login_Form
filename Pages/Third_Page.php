12bd42121e807d684e738a029be28cff17956045
cellum95 <101460624+cellum95@users.noreply.github.com>
2022-03-20 13:13:41 +0800

    Create Third_Page.php

---
+++ Pages/Third_Page.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    
+    <meta charset = "utf-8">
+    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    <title>Anomaly Sighting Forum</title>
+    <meta name = "description" content = "A webpage to store user data in the reposirtory">
+    <link href = "../CSS/style.php" rel = "stylesheet" type = "text/css">
+    
+</head>
+<body>
+
+	<header class = "header-title">
+		<h1>ANOMALY SIGHTING FORUM</h1>
+	</header>
+	
+	<main>
+
+	<h2>WELCOME TO THE ANOMALY SIGHTING FORUM</h2>
+		
+	</main>
+</body>
+</html>
