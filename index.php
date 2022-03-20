d2c3beb6fa0fc9dc7a91f33619394f3766247052
cellum95 <101460624+cellum95@users.noreply.github.com>
2022-03-20 20:18:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+	session_start();
+?>
+
 <!DOCTYPE html>
 
 <html>
@@ -20,21 +24,33 @@
 
 	<h2>LOG IN</h2>
 
-	<form action = "Pages/Third_Page.php" method = "post">
+	<form action = "index.php" method = "POST">
 		
 
 		<b>Username:</b><br>
-		<input type = "text" name = "username" style = "width : 95%;"><br>
+		<input type = "text" name = "u_name" style = "width : 95%;"><br>
 
 		<b>Password:</b><br>
-		<input type = "text" name = "password" style = "width : 95%;">
+		<input type = "password" name = "p_word" style = "width : 95%;">
 
 		<br>
 		<br>
 
-		<button name = "btn-submit" type = "submit" value = "Pages/Third_Page.php">Log In</button>
+		<input type = "hidden" name = "login_submit" value = "1">
+		<input type="submit" value="Login">
 		<a href = "Pages/Second_Page.php" class = "form_reg">Create Account</a>
 		
+		<?php
+			if(isset($_POST['login_submit'])) {
+				if($_POST['u_name'] != $_POST['u_name'] && $_POST['p_word'] != $_POST['rp_word']) {
+					echo "Username and/or password does not exist";
+				}
+				else {
+					header('Location: Pages/Third_Page.php');
+				}
+			}
+		?>
+
 	</form>
 		
 	</main>
